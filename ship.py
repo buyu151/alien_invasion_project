c7c8adfe9855798efff3b9098074b6e11f37a48b
Eduardo Nicolas Schulz <nicolas.schulz@proscientia.co.uk>
2022-04-07 17:06:02 +0100

    Added speed to settings.py and colission to left and right screen borders to ship.py

--- ship.py
+++ ship.py
@@ -6,11 +6,8 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
-        
-        #Movement flag
-        self.moving_right =  False
-        self.moving_left =  False
         
         #Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
@@ -19,14 +16,24 @@ class Ship:
         #Start each new ship at the bottom centre of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
         
-    def blitme(self):
-        """Draw the ship at tis current location."""
-        self.screen.blit(self.image, self.rect)
+        #Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
         
+        #Movement flag
+        self.moving_right =  False
+        self.moving_left =  False
+             
     def update(self):
         """Update the ship's position based on the movement flag."""
-        if self.moving_right:
-            self.rect.x += 1
-        elif self.moving_left:
-            self.rect.x -= 1
+        #Update the ship's x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        elif self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        #Update rect object from self.x.
+        self.rect.x = self.x
+    
+    def blitme(self):
+        """Draw the ship at tis current location."""
+        self.screen.blit(self.image, self.rect)        
         
