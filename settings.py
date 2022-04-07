18e48319e037a4784049d2fe2e04884dace5c250
Eduardo Nicolas Schulz <nicolas.schulz@proscientia.co.uk>
2022-04-07 19:00:11 +0100

    Created bullet class

--- settings.py
+++ settings.py
@@ -1,3 +1,6 @@
+from re import S
+
+
 class Settings:
     """A classs to store all settings for the game Alien Invasion."""
     
@@ -10,3 +13,11 @@ class Settings:
         
         #Ship settings.
         self.ship_speed = 1.5
+        
+        #Bullets settings.
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        
+        
