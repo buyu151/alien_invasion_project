9c46f59704900179f7d310a812ad25c2ed9913df
Eduardo Nicolas Schulz <nicolas.schulz@proscientia.co.uk>
2022-04-16 16:03:01 +0100

    Added collision between bullet and aliens sprites.

--- alien_invasion.py
+++ alien_invasion.py
@@ -90,6 +90,9 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
             #Check that the bullets are actually removed. 
             # print(len(self.bullets)) 
+        #Check for any bullets that have hit aliens.
+        #If so, get rid of both, the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
     
     def _update_aliens(self):
         """Check if the fleet is at an edge, then update the positions of all aliens in the fleet."""
