86c656ee5d3d4aaae10a944781c4682d47001dab
Eduardo Nicolas Schulz <nicolas.schulz@proscientia.co.uk>
2022-04-17 21:40:38 +0100

    Added mechanics for alien fleet reaching the bottom of the screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -120,6 +120,15 @@ class AlienInvasion:
         self.ship.center_ship()
         #Pause
         sleep(0.5)
+        
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen."""
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:    
+                #Treat this the same as if the ship got hit.
+                self._ship_hit()
+                break
       
     def _update_aliens(self):
         """Check if the fleet is at an edge, then update the positions of all aliens in the fleet."""
@@ -128,6 +137,8 @@ class AlienInvasion:
         #Look for alien-ship collisions.
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
             self._ship_hit()
+        #Look for aliens hitting the bottom of the screen. 
+        self._check_aliens_bottom()
             
     def _create_fleet(self):
         """Create the fleet of Aliens."""
